c885d7350b4eeac8f0ac91ce1d32b0e07edca84f
Saurabh Verma <saurabh1088@gmail.com>
2024-08-16 22:11:35 +0530

    Added props to welcome poster to showcase usage of props.

--- ReactNativeKickOffCLI/components/WelcomePoster.tsx
+++ ReactNativeKickOffCLI/components/WelcomePoster.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import type { PropsWithChildren } from "react";
 import { Text, Image } from "react-native";
 import welcomeStyle from "./WelcomeStyle";
 
-function WelcomePoster(): React.JSX.Element {
+type WelcomeProps = PropsWithChildren<{
+    name: string;
+    tagline: string;
+}>;
+
+/*
+Here we are using props and also giving type information for the prop which is requires as the project is using TypeScript.
+TypeScript add type safety on top of JavaScript hence this type declaration is required.
+*/
+function WelcomePoster(props: WelcomeProps): React.JSX.Element {
     return (
         <>
             <Image 
                 style={welcomeStyle.image} 
                 source={{uri: 'https://images.unsplash.com/photo-1593642634367-d91a135587b5'}}
             />
-            <Text>Welcome to learning React Native!!!</Text>
+            <Text>Welcome {props.name} to learning React Native!!!</Text>
+            <Text>{props.tagline}</Text>
         </>
     )
 }
