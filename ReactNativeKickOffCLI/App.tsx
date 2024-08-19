b9421c4df4d5af76dd5ecd7f2c385579535f6a00
Saurabh Verma <saurabh1088@gmail.com>
2024-08-19 22:46:46 +0530

    Added button examples.

--- ReactNativeKickOffCLI/App.tsx
+++ ReactNativeKickOffCLI/App.tsx
@@ -6,14 +6,37 @@
  */
 
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
 import WelcomePoster from './components/WelcomePoster';
 
+/*
+For Button, color for iOS is applied to the title, whereas for android it applies to background.
+*/
 function App(): React.JSX.Element {
+  const showAlert = () => {
+    Alert.alert("Pressed button one")
+  }
+
+  function showAnotherAlert() {
+    Alert.alert("Pressed button two")
+  }
+
   return (
     <SafeAreaView>
       <View style={{padding: 16}}>
         <WelcomePoster name='Batman' tagline='Because, I am BATMAN'/>
+        <View style={{flexDirection: 'row'}}>
+          <Button 
+            onPress={showAlert} 
+            title='Button One'
+            color={'red'}
+          />
+          <Button 
+            onPress={showAnotherAlert} 
+            title='Button Two'
+            color={'green'}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
