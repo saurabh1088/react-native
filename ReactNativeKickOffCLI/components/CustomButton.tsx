05723619fb887f82cd1087b09db46139d1c1e28b
Saurabh Verma <saurabh1088@gmail.com>
2024-08-20 08:29:43 +0530

    Initial commit for changes using Touchables.

---
+++ ReactNativeKickOffCLI/components/CustomButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Touchable, TouchableOpacity } from "react-native";
+
+function CustomButton(): React.JSX.Element {
+    return(
+        <TouchableOpacity></TouchableOpacity>
+    )
+}
