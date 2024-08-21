c283267d9f8c0a7409b9016937ea6d82803a412a
Saurabh Verma <saurabh1088@gmail.com>
2024-08-21 07:48:46 +0530

    Changes to wrap app code in NavigationContainer.

--- ReactNativeKickOffCLI/App.tsx
+++ ReactNativeKickOffCLI/App.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
 import WelcomePoster from './components/WelcomePoster';
 import CustomButton from './components/CustomButton';
+import { NavigationContainer } from '@react-navigation/native';
 
 /*
 For Button, color for iOS is applied to the title, whereas for android it applies to background.
@@ -27,24 +28,27 @@ function App(): React.JSX.Element {
   }
 
   return (
-    <SafeAreaView>
-      <View style={{padding: 16}}>
-        <WelcomePoster name='Batman' tagline='Because, I am BATMAN'/>
-        <View style={{flexDirection: 'row'}}>
-          <Button 
-            onPress={showAlert} 
-            title='Button One'
-            color={'red'}
-          />
-          <Button 
-            onPress={showAnotherAlert} 
-            title='Button Two'
-            color={'green'}
-          />
+    <NavigationContainer>
+      <SafeAreaView>
+        <View style={{padding: 16}}>
+          <WelcomePoster name='Batman' tagline='Because, I am BATMAN'/>
+          <View style={{flexDirection: 'row'}}>
+            <Button 
+              onPress={showAlert} 
+              title='Button One'
+              color={'red'}
+            />
+            <Button 
+              onPress={showAnotherAlert} 
+              title='Button Two'
+              color={'green'}
+            />
+          </View>
+          <CustomButton title='Custom Button' onPress={showAlertForCustomButton}/>
         </View>
-        <CustomButton title='Custom Button' onPress={showAlertForCustomButton}/>
-      </View>
-    </SafeAreaView>
+     </SafeAreaView>
+    </NavigationContainer>
+    
   );
 }
 
