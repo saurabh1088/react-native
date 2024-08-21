e46879dfbe03dd1ad13c8b7ff500af4fceaeb66a
Saurabh Verma <saurabh1088@gmail.com>
2024-08-21 22:43:24 +0530

    App changes to include navigation.

--- ReactNativeKickOffCLI/App.tsx
+++ ReactNativeKickOffCLI/App.tsx
@@ -10,42 +10,34 @@ import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
 import WelcomePoster from './components/WelcomePoster';
 import CustomButton from './components/CustomButton';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomePage from './screens/HomePage';
+import WelcomePage from './screens/WelcomePage';
 
 /*
 For Button, color for iOS is applied to the title, whereas for android it applies to background.
 */
+export type RootStackParamList = {
+  Home: undefined;
+  Welcome: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
 function App(): React.JSX.Element {
-  const showAlert = () => {
-    Alert.alert("Pressed button one")
-  }
-
-  function showAnotherAlert() {
-    Alert.alert("Pressed button two")
-  }
-
-  function showAlertForCustomButton() {
-    Alert.alert("Pressed custom button")
-  }
-
   return (
     <NavigationContainer>
       <SafeAreaView>
-        <View style={{padding: 16}}>
-          <WelcomePoster name='Batman' tagline='Because, I am BATMAN'/>
-          <View style={{flexDirection: 'row'}}>
-            <Button 
-              onPress={showAlert} 
-              title='Button One'
-              color={'red'}
-            />
-            <Button 
-              onPress={showAnotherAlert} 
-              title='Button Two'
-              color={'green'}
-            />
-          </View>
-          <CustomButton title='Custom Button' onPress={showAlertForCustomButton}/>
-        </View>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={HomePage}
+          />
+          <Stack.Screen
+            name='Welcome'
+            component={WelcomePage}
+          />
+        </Stack.Navigator>
      </SafeAreaView>
     </NavigationContainer>
     
