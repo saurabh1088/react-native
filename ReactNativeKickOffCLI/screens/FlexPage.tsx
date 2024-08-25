59661dfc8d579ebb60c2b1c2c7d2d94b00694691
Saurabh Verma <saurabh1088@gmail.com>
2024-08-25 15:22:35 +0530

    Added section comments for flex example views.

--- ReactNativeKickOffCLI/screens/FlexPage.tsx
+++ ReactNativeKickOffCLI/screens/FlexPage.tsx
@@ -9,14 +9,14 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
     return (
         <View>
             <SafeAreaView>
-                <Text>Default Flex Behaviour</Text>
+                <Text>Default Flex Behaviour : Arranged in column</Text>
                 <View style={{width: 100, height: 300}}>
                     <View style={{flex: 1, backgroundColor: 'red'}}></View>
                     <View style={{flex: 1, backgroundColor: 'blue'}}></View>
                     <View style={{flex: 1, backgroundColor: 'green'}}></View>
                 </View>
 
-                <Text>Flex Row</Text>
+                <Text>Flex customised for row alignment</Text>
                 <View style={{flexDirection: 'row', width: 300, height: 100}}>
                     <View style={{flex: 1, backgroundColor: 'red'}}></View>
                     <View style={{flex: 1, backgroundColor: 'blue'}}></View>
