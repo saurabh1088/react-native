1a187901bbea5ffa1b5d6d333bb077715840af0b
Saurabh Verma <saurabh1088@gmail.com>
2024-08-25 18:54:54 +0530

    Added comments about flex examples.

--- ReactNativeKickOffCLI/screens/FlexPage.tsx
+++ ReactNativeKickOffCLI/screens/FlexPage.tsx
@@ -9,7 +9,9 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
     return (
         <View>
             <SafeAreaView>
+                {/* ScrollView will load all the elements, for lazy loading FlatList can be used */}
                 <ScrollView>
+                    {/* By default flex in mobile platforms will align elements in column */}
                     <Text>Default Flex Behaviour : Arranged in column</Text>
                     <View style={{width: 100, height: 300}}>
                         <View style={{flex: 1, backgroundColor: 'red'}}></View>
@@ -17,6 +19,7 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
                         <View style={{flex: 1, backgroundColor: 'green'}}></View>
                     </View>
 
+                    {/* To align elements in row flexDirection needs to be set to row */}
                     <Text>Flex customised for row alignment</Text>
                     <View style={{flexDirection: 'row', width: 300, height: 100}}>
                         <View style={{flex: 1, backgroundColor: 'red'}}></View>
@@ -24,6 +27,10 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
                         <View style={{flex: 1, backgroundColor: 'green'}}></View>
                     </View>
 
+                    {/* Here flexDirection is row, so the three elements will be filled in ratio as per flex value
+                        For example total value here is 1+2+4 = 7, so ration in which width will be divided will be 
+                        1/7, 2/7 and 4/7
+                     */}
                     <Text>Flex customised for different fill</Text>
                     <View style={{flexDirection: 'row', width: 300, height: 100}}>
                         <View style={{flex: 1, backgroundColor: 'red'}}></View>
@@ -31,6 +38,9 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
                         <View style={{flex: 4, backgroundColor: 'green'}}></View>
                     </View>
 
+                    {/* flexDirection here is set to row reverse, means all elements will be arranged in row, in the
+                        reverse direction of which those are declared.
+                    */}
                     <Text>Flex customised for reverse direction</Text>
                     <View style={{flexDirection: 'row-reverse', width: 300, height: 100}}>
                         <View style={{flex: 1, backgroundColor: 'red'}}></View>
