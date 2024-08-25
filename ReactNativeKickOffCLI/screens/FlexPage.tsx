086331dda34787f22d1f0d28690cdbb229e6dbb1
Saurabh Verma <saurabh1088@gmail.com>
2024-08-25 16:58:34 +0530

    Changes to add example for flex customisation for different fill and reverse direction.

--- ReactNativeKickOffCLI/screens/FlexPage.tsx
+++ ReactNativeKickOffCLI/screens/FlexPage.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native"
+import { ScrollView, Text, View } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { RootStackParamList } from "../App";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,19 +9,35 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
     return (
         <View>
             <SafeAreaView>
-                <Text>Default Flex Behaviour : Arranged in column</Text>
-                <View style={{width: 100, height: 300}}>
-                    <View style={{flex: 1, backgroundColor: 'red'}}></View>
-                    <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-                    <View style={{flex: 1, backgroundColor: 'green'}}></View>
-                </View>
+                <ScrollView>
+                    <Text>Default Flex Behaviour : Arranged in column</Text>
+                    <View style={{width: 100, height: 300}}>
+                        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+                        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                    </View>
 
-                <Text>Flex customised for row alignment</Text>
-                <View style={{flexDirection: 'row', width: 300, height: 100}}>
-                    <View style={{flex: 1, backgroundColor: 'red'}}></View>
-                    <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-                    <View style={{flex: 1, backgroundColor: 'green'}}></View>
-                </View>
+                    <Text>Flex customised for row alignment</Text>
+                    <View style={{flexDirection: 'row', width: 300, height: 100}}>
+                        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+                        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                    </View>
+
+                    <Text>Flex customised for different fill</Text>
+                    <View style={{flexDirection: 'row', width: 300, height: 100}}>
+                        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                        <View style={{flex: 2, backgroundColor: 'blue'}}></View>
+                        <View style={{flex: 4, backgroundColor: 'green'}}></View>
+                    </View>
+
+                    <Text>Flex customised for reverse direction</Text>
+                    <View style={{flexDirection: 'row-reverse', width: 300, height: 100}}>
+                        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                        <View style={{flex: 2, backgroundColor: 'blue'}}></View>
+                        <View style={{flex: 4, backgroundColor: 'green'}}></View>
+                    </View>
+                </ScrollView>
             </SafeAreaView>
         </View>
     )
