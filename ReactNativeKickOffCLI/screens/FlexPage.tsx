c87ce9c74530509e9e52ad9bae2454785f06c060
Saurabh Verma <saurabh1088@gmail.com>
2024-08-25 12:21:35 +0530

    Added example for default flex behaviour which defaults to column.

--- ReactNativeKickOffCLI/screens/FlexPage.tsx
+++ ReactNativeKickOffCLI/screens/FlexPage.tsx
@@ -1,12 +1,21 @@
 import { View } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { RootStackParamList } from "../App";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export type FlexPageNavigationProp = NativeStackScreenProps<RootStackParamList, 'FlexPage'>;
 
-const FlexPage = ({}) => {
+const FlexPage = ({navigation}: FlexPageNavigationProp) => {
     return (
-        <View></View>
+        <View>
+            <SafeAreaView>
+                <View style={{width: 100, height: 300}}>
+                    <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                    <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+                    <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                </View>
+            </SafeAreaView>
+        </View>
     )
 }
 
