6428f6d8f1fb0bfaa3591026793dc2ddd768fd01
Saurabh Verma <saurabh1088@gmail.com>
2024-08-25 15:21:20 +0530

    Added flex example to arrange view in row.

--- ReactNativeKickOffCLI/screens/FlexPage.tsx
+++ ReactNativeKickOffCLI/screens/FlexPage.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { Text, View } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { RootStackParamList } from "../App";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,7 +9,15 @@ const FlexPage = ({navigation}: FlexPageNavigationProp) => {
     return (
         <View>
             <SafeAreaView>
+                <Text>Default Flex Behaviour</Text>
                 <View style={{width: 100, height: 300}}>
+                    <View style={{flex: 1, backgroundColor: 'red'}}></View>
+                    <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+                    <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                </View>
+
+                <Text>Flex Row</Text>
+                <View style={{flexDirection: 'row', width: 300, height: 100}}>
                     <View style={{flex: 1, backgroundColor: 'red'}}></View>
                     <View style={{flex: 1, backgroundColor: 'blue'}}></View>
                     <View style={{flex: 1, backgroundColor: 'green'}}></View>
