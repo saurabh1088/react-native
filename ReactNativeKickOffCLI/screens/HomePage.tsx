f173cd401930825e9413c77d5768b8c7a684c7fb
Saurabh Verma <saurabh1088@gmail.com>
2024-10-28 21:55:07 +0530

    Added modal view example.

--- ReactNativeKickOffCLI/screens/HomePage.tsx
+++ ReactNativeKickOffCLI/screens/HomePage.tsx
@@ -1,11 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { Button } from "react-native"
+import { Button, Modal, Text, View } from "react-native"
 import { RootStackParamList } from "../App";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export type HomeScreenNavigationProp = NativeStackScreenProps<RootStackParamList, 'HomePage'>;
 
 const HomePage = ({navigation}: HomeScreenNavigationProp) => {
+    const [showModalView, setShowModalView] = useState(false)
     return (
         <SafeAreaView>
             <Button
@@ -26,6 +28,23 @@ const HomePage = ({navigation}: HomeScreenNavigationProp) => {
                     navigation.navigate('FlexPage')
                 }
             />
+            <Button
+                title="Modal"
+                onPress={() =>
+                    setShowModalView(true)
+                }
+            />
+            <Modal animationType="slide" visible={showModalView}>
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <Text>This is a modal view</Text>
+                    <Button
+                        title="Dismiss"
+                        onPress={() =>
+                            setShowModalView(false)
+                        }
+                    />
+                </View>
+            </Modal>
         </SafeAreaView>
     )
 }
