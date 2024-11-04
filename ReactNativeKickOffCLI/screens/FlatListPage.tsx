9747484701cc1b87785272e633551253b9391444
Saurabh Verma <saurabh1088@gmail.com>
2024-11-04 20:33:33 +0530

    Added more data set.

--- ReactNativeKickOffCLI/screens/FlatListPage.tsx
+++ ReactNativeKickOffCLI/screens/FlatListPage.tsx
@@ -19,6 +19,12 @@ const FlatListPage = ({navigation}: FlatListPageNavigationProp) => {
         { id: '2', name: 'Superman', powers: 'Fly, strongest and laser eyes' },
         { id: '3', name: 'Wonder Woman', powers: 'Lasso, power bracelets and immortal' },
         { id: '4', name: 'Flash', powers: 'Super fast' },
+        { id: '5', name: 'Aquaman', powers: 'Underwater breathing and control of sea creatures' },
+        { id: '6', name: 'Cyborg', powers: 'Enhanced strength, technopathy, and advanced technology' },
+        { id: '7', name: 'Green Lantern', powers: 'Power ring that creates energy constructs' },
+        { id: '8', name: 'Martian Manhunter', powers: 'Shape-shifting, telepathy, and invisibility' },
+        { id: '9', name: 'Green Arrow', powers: 'Master archer and tactician' },
+        { id: '10', name: 'Hawkgirl', powers: 'Flight, combat skills, and Nth metal weaponry' },
     ];
 
     const renderSuperHero = ({ item }: { item: DCHero }) => (
