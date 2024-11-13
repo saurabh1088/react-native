e5343f783e3bbf2ba6216178e5aa44307d938f9d
Saurabh Verma <saurabh1088@gmail.com>
2024-11-13 20:03:47 +0530

    Fixed issue with cyclic import.

--- ReactNativeKickOffCLI/components/ImageLabelDetailCell.tsx
+++ ReactNativeKickOffCLI/components/ImageLabelDetailCell.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import type { PropsWithChildren } from "react";
 import {  Text, View, Image } from "react-native";
 import imageLabelDetailCellStyle from "./ImageLabelDetailCellStyle";
-import { images } from "../screens/FlatListPage"
 
 type ImageLabelDetailCellProps = PropsWithChildren<{
     label: string,
     detail: string,
     imageName: string
 }>;
+
+export const images: { [key: string]: any } = {
+    aquaman: require('../assets/images/dc_aquaman.jpeg'),
+    batman: require('../assets/images/dc_batman.jpeg'),
+    cyborg: require('../assets/images/dc_cyborg.jpeg'),
+    flash: require('../assets/images/dc_flash.jpeg'),
+    greenArrow: require('../assets/images/dc_green_arrow.jpeg'),
+    greenLantern: require('../assets/images/dc_green_lantern.jpeg'),
+    hawkgirl: require('../assets/images/dc_hawkgirl.jpeg'),
+    martianManhunter: require('../assets/images/dc_martian_manhunter.jpeg'),
+    superman: require('../assets/images/dc_superman.jpeg'),
+    wonderWoman: require('../assets/images/dc_wonder_woman.jpeg'),
+};
 
 function ImageLabelDetailCell(props: ImageLabelDetailCellProps): React.JSX.Element {
     return(
--- ReactNativeKickOffCLI/screens/FlatListPage.tsx
+++ ReactNativeKickOffCLI/screens/FlatListPage.tsx
@@ -13,19 +13,6 @@ type DCHero = {
     iconName: string;
 }
 
-export const images: { [key: string]: any } = {
-    aquaman: require('../assets/images/dc_aquaman.jpeg'),
-    batman: require('../assets/images/dc_batman.jpeg'),
-    cyborg: require('../assets/images/dc_cyborg.jpeg'),
-    flash: require('../assets/images/dc_flash.jpeg'),
-    greenArrow: require('../assets/images/dc_green_arrow.jpeg'),
-    greenLantern: require('../assets/images/dc_green_lantern.jpeg'),
-    hawkgirl: require('../assets/images/dc_hawkgirl.jpeg'),
-    martianManhunter: require('../assets/images/dc_martian_manhunter.jpeg'),
-    superman: require('../assets/images/dc_superman.jpeg'),
-    wonderWoman: require('../assets/images/dc_wonder_woman.jpeg'),
-};
-
 const FlatListPage = ({navigation}: FlatListPageNavigationProp) => {
 
     // Sample data for Flat List
